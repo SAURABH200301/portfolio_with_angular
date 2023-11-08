4a8dabfeb2aa6e7ae2999d7d3aeb021f95c72e00
SAURABH200301 <saurabhsharma200301@gmail.com>
2023-11-08 18:49:06 +0530

    added more changes

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import { Projects, projects } from './projects';
+// import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  // animations: [
+  //   trigger('imageZoom', [
+  //     state('normal', style({ transform: 'scale(1)' })),
+  //     state('zoomed', style({ transform: 'scale(1.2)' })),
+  //     transition('normal => zoomed', animate('0.2s')),
+  //     transition('zoomed => normal', animate('0.2s'))
+  //   ])
+  // ]
 })
 export class ProjectsComponent {
   projects: projects[] = Projects;
-  isHovered: boolean = false;
 
-  onMouseEnter() {
-    this.isHovered = true;
+  onMouseEnter(pro:projects) {
+    pro.isHovered = true;
   }
-  onMouseLeave() {
-    this.isHovered = false;
+  onMouseLeave(pro:projects) {
+    pro.isHovered = false;
   }
 }
--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,51 +1,99 @@
+interface liveLinks {
+    linkIcon: string,
+    link: string
+}
 export interface projects {
     image: string,
     head: string,
     desc: string,
     techUsed: string[],
-    LiveLink:string,
-    createdDate:Date
+    LiveLink: liveLinks[],
+    createdDate: Date,
+    isHovered: boolean
 }
 
 export const Projects: projects[] = [
     {
-        image: '/assets/icons/chat_app.gif',
-        head:'Join - A chatting app',
-        desc:' Join is a chat application',
-        techUsed:['ReactJs','Firebase','NodeJS'],
-        LiveLink:'none',
-        createdDate:new Date('2022-01-17')
-    },
-    {
         image: '/assets/icons/Tasky.gif',
-        head:'Task management app',
-        desc:' task management app given you feature of authentication, CRUD, function of MONGODB',
-        techUsed:['ReactJs','MongoDB','NodeJS','Express'],
-        LiveLink:'none',
-        createdDate:new Date('2023-09-09')
-    },
-    {
-        image: '/assets/icons/box_office.gif',
-        head:'Box Office',
-        desc:' Box Office is a webapp where you can search about actors and movies and make them starred',
-        techUsed:['ReactJs','APIs'],
-        LiveLink:'none',
-        createdDate:new Date('2022-01-06')
+        head: 'Task management app',
+        desc: ' task management app given you feature of authentication, CRUD, function of MONGODB',
+        techUsed: ['/assets/icons/react.png', '/assets/icons/mongo.png', '/assets/icons/node.png', '/assets/icons/express.png'],
+        LiveLink: [
+            {
+                linkIcon: "/assets/icons/github.png",
+                link: "https://github.com/SAURABH200301/Task-Management-app"
+            }
+        ],
+        createdDate: new Date('2023-09-09'),
+        isHovered: false
     },
     {
         image: '/assets/icons/newsapp.gif',
-        head:'NEWS APP',
-        desc:' News app is an application where you can read live news',
-        techUsed:['ReactJs','APIs','BootStrap'],
-        LiveLink:'none',
-        createdDate:new Date('2022-03-07')
+        head: 'NEWS APP',
+        desc: ' News app is an application where you can read live news',
+        techUsed: ['/assets/icons/react.png', '/assets/icons/api.png', '/assets/icons/BS.png'],
+        LiveLink: [
+            {
+                linkIcon: "/assets/icons/github.png",
+                link: "https://github.com/SAURABH200301/News_App"
+            },
+            {
+                linkIcon: "/assets/icons/link.png",
+                link: "https://github.com/SAURABH200301/News_App"
+            }
+        ],
+        createdDate: new Date('2022-03-07'),
+        isHovered: false
     },
     {
+        image: '/assets/icons/chat_app.gif',
+        head: 'Join - A chatting app',
+        desc: ' Join is a chat application',
+        techUsed: ['/assets/icons/react.png', '/assets/icons/firebase.png', '/assets/icons/node.png'],
+        LiveLink: [
+            {
+                linkIcon: "/assets/icons/github.png",
+                link: "https://github.com/SAURABH200301/Chat-App"
+            },
+            {
+                linkIcon: "/assets/icons/link.png",
+                link: "https://chat-app-49705.web.app/"
+            }
+        ],
+        createdDate: new Date('2022-01-17'),
+        isHovered: false
+    },
+    {
+        image: '/assets/icons/box_office.gif',
+        head: 'Box Office',
+        desc: ' Box Office is a webapp where you can search about actors and movies and make them starred',
+        techUsed: ['/assets/icons/react.png', '/assets/icons/api.png'],
+        LiveLink: [
+            {
+                linkIcon: "/assets/icons/github.png",
+                link: "https://github.com/SAURABH200301/box-office"
+            },
+            {
+                linkIcon: "/assets/icons/link.png",
+                link: "https://box-office-steel.vercel.app/"
+            }
+        ],
+        createdDate: new Date('2022-01-06'),
+        isHovered: false
+    },
+    
+    {
         image: '/assets/icons/musicplayer.gif',
-        head:'Music Player',
-        desc:' Music player is demo of todays spotify not same. ',
-        techUsed:['HTML','CSS','JS'],
-        LiveLink:'none',
-        createdDate:new Date('2022-06-27')
+        head: 'Music Player',
+        desc: ' Music player is demo of todays spotify not same. ',
+        techUsed: ['/assets/icons/html.png', '/assets/icons/css.png', '/assets/icons/js.png'],
+        LiveLink: [
+            {
+                linkIcon: "/assets/icons/github.png",
+                link: "https://github.com/SAURABH200301/MusicPlayer"
+            }
+        ],
+        createdDate: new Date('2021-06-27'),
+        isHovered: false
     }
 ]
